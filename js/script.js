4cdd3f385925b0f82057717645125f09605168f5
Emmanuela Joy <emmajoy81@gmail.com>
2021-03-08 07:56:37 +0300

    add an order form

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+    //toggle function for the pizza-slice icon that displays on smaller screens
     $('.fa-pizza-slice').click(function () {
         $(this).toggleClass('fa-times');
         $('nav').toggleClass('nav-toggle');
@@ -9,6 +10,7 @@ $(document).ready(function () {
         $('nav').removeClass('nav-toggle');
     });
 
+    //a function that displays a different image on the landing page when someone clicks on the dots below
     $('.dot1').click(function () {
         $('#img1').css('display', 'block');
         $('#img2').css('display', 'none');
@@ -27,6 +29,7 @@ $(document).ready(function () {
         $('#img2').css('display', 'none');
     });
 
+    //a function that makes the header position sticky
     $(window).on('scroll load', function () {
         if ($(window).scrollTop() > 10) {
             $('#header').addClass('header-active');
@@ -35,6 +38,7 @@ $(document).ready(function () {
         }
     });
 
+    //a hover function for the menu items
     $(".card-img-top").hover(function () {
         $(this).stop().animate({
             opacity: .6
@@ -46,5 +50,30 @@ $(document).ready(function () {
         }, 500);
         $('.price').css('visibility', 'hidden');
     });
+
+
+    //declaring an object called pizzasize
+    let pizzaSize = {
+        small: 850,
+        medium: 1150,
+        large: 1300
+    }
+
+    //pizza constructor
+    function Pizza(name, size, crust, toppings) {
+        this.pizzaName = name;
+        this.pizzaSize = size;
+        this.pizzaCrust = crust;
+        this.pizzaToppings = toppings;
+    }
+    var order1 = new Pizza("bbq", "small", "thick", "bacon");
+
+    /*let pizza = {
+        name: [],
+        pizzaSize,
+        crust: [Crispy, Stuffed, Gluten - free],
+        toppings: [Salami, Mushroom, Mozzarella, Onions, Bacon]
+    }*/
+
 
 });
